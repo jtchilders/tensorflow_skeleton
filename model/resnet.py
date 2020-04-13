3899b52a2aaea00212df1546c66e0f4dcf3b8bc3
Taylor Childers <taylor@jtchilders.com>
2020-04-13 18:13:24 +0000

    fixes to model

--- model/resnet.py
+++ model/resnet.py
@@ -9,49 +9,63 @@ class ResNet50(tf.keras.Model):
    def __init__(self,config):
       super(ResNet50,self).__init__(name='')
 
-      self.conv2a = tf.keras.layers.Conv2D(config['data']['num_channels'], (1, 1))
-      self.bn2a = tf.keras.layers.BatchNormalization()
+      self.conv2a = tf.keras.layers.Conv2D(64, (7, 7),strides=(2,2),padding='same',name='conv_input')
+      self.bn2a = tf.keras.layers.BatchNormalization(name='bn_input')
 
       self.poolA = tf.keras.layers.MaxPool2D((3,3),(2,2))
 
-      self.cbA = ResnetConvBlock(3,[64,64,256])
-      self.idAa = ResnetIdentityBlock(3,[64,64,256])
-      self.idAb = ResnetIdentityBlock(3,[64,64,256])
+      self.cbA = ResnetConvBlock(3,[64,64,256],stage=2,block='a',strides=(1,1))
+      self.idAa = ResnetIdentityBlock(3,[64,64,256],stage=2,block='b')
+      self.idAb = ResnetIdentityBlock(3,[64,64,256],stage=2,block='c')
 
-      self.cbB = ResnetConvBlock(3,[128, 128, 512])
-      self.idBa = ResnetIdentityBlock(3,[128, 128, 512])
-      self.idBb = ResnetIdentityBlock(3,[128, 128, 512])
-      self.idBc = ResnetIdentityBlock(3,[128, 128, 512])
+      self.cbB = ResnetConvBlock(3,[128, 128, 512],stage=3,block='a')
+      self.idBa = ResnetIdentityBlock(3,[128, 128, 512],stage=3,block='b')
+      self.idBb = ResnetIdentityBlock(3,[128, 128, 512],stage=3,block='c')
+      self.idBc = ResnetIdentityBlock(3,[128, 128, 512],stage=3,block='d')
 
-      self.cbC = ResnetConvBlock(3,[256, 256, 1024])
-      self.idCa = ResnetIdentityBlock(3,[256, 256, 1024])
-      self.idCb = ResnetIdentityBlock(3,[256, 256, 1024])
-      self.idCc = ResnetIdentityBlock(3,[256, 256, 1024])
-      self.idCd = ResnetIdentityBlock(3,[256, 256, 1024])
-      self.idCe = ResnetIdentityBlock(3,[256, 256, 1024])
+      self.cbC = ResnetConvBlock(3,[256, 256, 1024],stage=4,block='a')
+      self.idCa = ResnetIdentityBlock(3,[256, 256, 1024],stage=4,block='b')
+      self.idCb = ResnetIdentityBlock(3,[256, 256, 1024],stage=4,block='c')
+      self.idCc = ResnetIdentityBlock(3,[256, 256, 1024],stage=4,block='d')
+      self.idCd = ResnetIdentityBlock(3,[256, 256, 1024],stage=4,block='e')
+      self.idCe = ResnetIdentityBlock(3,[256, 256, 1024],stage=4,block='f')
 
-      self.cbD = ResnetConvBlock(3,[512, 512, 2048])
-      self.idDa = ResnetIdentityBlock(3,[512, 512, 2048])
-      self.idDb = ResnetIdentityBlock(3,[512, 512, 2048])
+      self.cbD = ResnetConvBlock(3,[512, 512, 2048],stage=5,block='a')
+      self.idDa = ResnetIdentityBlock(3,[512, 512, 2048],stage=5,block='b')
+      self.idDb = ResnetIdentityBlock(3,[512, 512, 2048],stage=5,block='c')
 
-      self.poolB = tf.keras.layers.AveragePooling2D((2,2))
+      self.poolB = tf.keras.layers.AveragePooling2D((7,7),strides=(7,7))
 
       self.flatten = tf.keras.layers.Flatten()
 
-      self.dense = tf.keras.layers.Dense(config['data']['num_classes'])
+      self.dense = tf.keras.layers.Dense(config['data']['num_classes'],name='fc%s' % config['data']['num_classes'])
 
    def call(self, input_tensor, training=False):
-      x = tf.nn.relu(self.bn2a(self.conv2a(input_tensor)))
+      x = self.conv2a(input_tensor)
+      x = self.bn2a(x)
+      x = tf.nn.relu(x)
 
       x = self.poolA(x)
 
-      x = self.idAb(self.idAa(self.cbA(x,training),training),training)
+      x = self.cbA(x,training)
+      x = self.idAa(x,training)
+      x = self.idAb(x,training)
 
-      x = self.idBc(self.idBb(self.idBa(self.cbB(x,training),training),training),training)
+      x = self.cbB(x,training)
+      x = self.idBa(x,training)
+      x = self.idBb(x,training)
+      x = self.idBc(x,training)
 
-      x = self.idCe(self.idCd(self.idCc(self.idCb(self.idCa(self.cbC(x,training),training),training),training),training),training)
+      x = self.cbC(x,training)
+      x = self.idCa(x,training)
+      x = self.idCb(x,training)
+      x = self.idCc(x,training)
+      x = self.idCd(x,training)
+      x = self.idCe(x,training)
 
-      x = self.idDb(self.idDa(self.cbD(x,training),training),training)
+      x = self.cbD(x,training)
+      x = self.idDa(x,training)
+      x = self.idDb(x,training)
 
       x = self.poolB(x)
 
@@ -60,19 +74,23 @@ class ResNet50(tf.keras.Model):
       return self.dense(x)
 
 
-class ResnetIdentityBlock(tf.keras.layers.Layer):
-   def __init__(self, kernel_size, filters):
+class ResnetIdentityBlock(tf.keras.Model):
+   def __init__(self, kernel_size, filters, stage, block,data_format='channels_last'):
       super(ResnetIdentityBlock, self).__init__(name='')
       filters1, filters2, filters3 = filters
 
-      self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
-      self.bn2a = tf.keras.layers.BatchNormalization()
+      conv_name_base = 'res' + str(stage) + block + '_branch'
+      bn_name_base = 'bn' + str(stage) + block + '_branch'
+      bn_axis = 1 if data_format == 'channels_first' else 3
 
-      self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
-      self.bn2b = tf.keras.layers.BatchNormalization()
+      self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1),name=conv_name_base + '_2a', data_format=data_format)
+      self.bn2a = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2a')
 
-      self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
-      self.bn2c = tf.keras.layers.BatchNormalization()
+      self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same',name=conv_name_base + '_2b', data_format=data_format)
+      self.bn2b = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2b')
+
+      self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1),name=conv_name_base + '_2c', data_format=data_format)
+      self.bn2c = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2c')
 
    def call(self, input_tensor, training=False):
       x = self.conv2a(input_tensor)
@@ -90,22 +108,26 @@ class ResnetIdentityBlock(tf.keras.layers.Layer):
       return tf.nn.relu(x)
 
 
-class ResnetConvBlock(tf.keras.layers.Layer):
-   def __init__(self, kernel_size, filters):
+class ResnetConvBlock(tf.keras.Model):
+   def __init__(self, kernel_size, filters, stage, block, data_format='channels_last', strides=(2, 2)):
       super(ResnetConvBlock, self).__init__(name='')
       filters1, filters2, filters3 = filters
 
-      self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
-      self.bn2a = tf.keras.layers.BatchNormalization()
+      conv_name_base = 'res' + str(stage) + block + '_branch'
+      bn_name_base = 'bn' + str(stage) + block + '_branch'
+      bn_axis = 1 if data_format == 'channels_first' else 3
 
-      self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
-      self.bn2b = tf.keras.layers.BatchNormalization()
+      self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1),strides=strides,name=conv_name_base + '_2a', data_format=data_format)
+      self.bn2a = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2a')
 
-      self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
-      self.bn2c = tf.keras.layers.BatchNormalization()
+      self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same',name=conv_name_base + '_2b', data_format=data_format)
+      self.bn2b = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2b')
 
-      self.conv2d = tf.keras.layers.Conv2D(filters3, (1, 1))
-      self.bn2d = tf.keras.layers.BatchNormalization()
+      self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1),name=conv_name_base + '_2c', data_format=data_format)
+      self.bn2c = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2c')
+
+      self.conv2d = tf.keras.layers.Conv2D(filters3, (1, 1),strides=strides,name=conv_name_base + '_2d', data_format=data_format)
+      self.bn2d = tf.keras.layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '_2d')
 
    def call(self, input_tensor, training=False):
       x = self.conv2a(input_tensor)
@@ -119,6 +141,9 @@ class ResnetConvBlock(tf.keras.layers.Layer):
       x = self.conv2c(x)
       x = self.bn2c(x, training=training)
 
-      x += self.bn2d(self.conv2d(input_tensor))
+      shortcut = self.conv2d(input_tensor)
+      shortcut = self.bn2d(shortcut)
+
+      x += shortcut
 
       return tf.nn.relu(x)
