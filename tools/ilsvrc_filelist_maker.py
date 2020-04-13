115a6c6b4e8744276c96645d933a16e5ffca56d5
Taylor Childers <taylor@jtchilders.com>
2020-04-13 18:12:53 +0000

    adding tool to make filelist

---
+++ tools/ilsvrc_filelist_maker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import argparse,logging,glob
+logger = logging.getLogger(__name__)
+
+
+def main():
+   ''' simple starter program that can be copied for use when starting a new script. '''
+   logging_format = '%(asctime)s %(levelname)s:%(name)s:%(message)s'
+   logging_datefmt = '%Y-%m-%d %H:%M:%S'
+   logging_level = logging.INFO
+   
+   parser = argparse.ArgumentParser(description='')
+   parser.add_argument('-g','--glob',help='glob string to use to seach and dump files',required=True)
+   parser.add_argument('-o','--output',help='output filename',required=True)
+   parser.add_argument('-r','--recursive',default=False,action='store_true',help='glob recursively')
+
+   parser.add_argument('--debug', dest='debug', default=False, action='store_true', help="Set Logger to DEBUG")
+   parser.add_argument('--error', dest='error', default=False, action='store_true', help="Set Logger to ERROR")
+   parser.add_argument('--warning', dest='warning', default=False, action='store_true', help="Set Logger to ERROR")
+   parser.add_argument('--logfilename',dest='logfilename',default=None,help='if set, logging information will go to file')
+   args = parser.parse_args()
+
+   if args.debug and not args.error and not args.warning:
+      logging_level = logging.DEBUG
+   elif not args.debug and args.error and not args.warning:
+      logging_level = logging.ERROR
+   elif not args.debug and not args.error and args.warning:
+      logging_level = logging.WARNING
+
+   logging.basicConfig(level=logging_level,
+                       format=logging_format,
+                       datefmt=logging_datefmt,
+                       filename=args.logfilename)
+   
+   filelist = glob.glob(args.glob,recursive=args.recursive)
+
+   with open(args.output,'w') as file:
+      for filename in filelist:
+         file.write(filename + '\n')
+
+   
+
+
+
+if __name__ == "__main__":
+   main()
