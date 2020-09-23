e75c56c4f4f3a246d78c51197e1ec866bf99f6c8
Taylor Childers <taylor@jtchilders.com>
2020-09-23 14:26:50 +0000

    fix home directory mismatching /gpfs/mira-home/ vs /home/

--- submit_scripts/thetagpu_mnist.sh
+++ submit_scripts/thetagpu_mnist.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# get current folder containing this script
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# convert /gpfs/mira-home/ to /home/
+DIR=$( echo $DIR | sed "s/\/gpfs\/mira-/\//g" )
+echo DIR=$DIR
 MPIPATH=/usr/mpi/gcc/openmpi-4.0.3rc4
 
 echo COBALT_NODEFILE=$COBALT_NODEFILE
@@ -16,9 +20,16 @@ PPN=8
 PROCS=$((NODES * PPN))
 echo NODES=$NODES  PPN=$PPN  PROCS=$PROCS
 
+# MNIST expects the mnist.tgz file to be in $HOME/.keras/datasets
+if [ ! -d $HOME/.keras/datasets/mnist.tgz ]; then
+   mkdir -p $HOME/.keras/datasets
+   cp $DIR/../datasets/mnist.tgz $HOME/.keras/datasets/
+fi
+
+
 CONTAINER=/home/parton/tensorflow-20.08-tf2-py3.simg
 
 mpirun -hostfile $COBALT_NODEFILE -n $PROCS -npernode $PPN  \
   singularity exec --nv $CONTAINER  \
     python $DIR/../main.py -c $DIR/../configs/mnist.json \
-    --logdir logdir/$COBALT_JOBID --intraop 16 --interop 16 --horovod
+    --logdir $DIR/../logdir/$COBALT_JOBID --intraop 16 --interop 16 --horovod
