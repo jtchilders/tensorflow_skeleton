c7da7c4430b85421d923dc47d4f8d202ed9735ac
Taylor Childers <taylor@jtchilders.com>
2020-12-08 15:22:33 +0000

    update submit script

--- submit_scripts/theta_ilsvrc_resnet.sh
+++ submit_scripts/theta_ilsvrc_resnet.sh
@@ -3,9 +3,9 @@
 #COBALT -t 30
 #COBALT -q debug-flat-quad
 #COBALT -A datascience
+#COBALT -O logdir/$COBALT_JOBID
 
 echo [$SECONDS] setup conda environment
-#source /projects/datascience/parton/conda/miniconda3/latest/setup.sh
 module load miniconda-3/2020-12
 
 echo [$SECONDS] setup local env vars
@@ -23,13 +23,12 @@ fi
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_BLOCKTIME=0
 
-export MKL_VERBOSE=1
+#export MKL_VERBOSE=1
 #export DNNL_VERBOSE=1
 #export ONEDNN_VERBOSE=1
-export dnnl_verbose=1
-#export MKLDNN_VERBOSE=1
+#export dnnl_verbose=1
 #-d $OMP_NUM_THREADS -j $HYPERTHREADS_PER_CORE --cc depth
-env | sort > $COBALT_JOBID.env
+env | sort > logdir/$COBALT_JOBID.env
 aprun -n $RANKS -N $RANKS_PER_NODE --cc none \
    python main.py -c configs/ilsvrc.json --logdir logdir/$COBALT_JOBID --intraop $OMP_NUM_THREADS --interop $OMP_NUM_THREADS --profiler --batch-term 50 $HOROVOD
 
