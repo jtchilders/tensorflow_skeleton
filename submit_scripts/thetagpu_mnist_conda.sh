34d912c32d635ecd3a2145100bf0505c85a7ce1b
Taylor Childers <taylorchilders@gmail.com>
2024-10-22 14:25:29 -0500

    adding last used setup file

--- submit_scripts/thetagpu_mnist_conda.sh
+++ submit_scripts/thetagpu_mnist_conda.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#COBALT -n 1
+#COBALT -t 30
+#COBALT -A datascience
+#COBALT -O logdir/$COBALT_JOBID
 
-source /lus/theta-fs0/projects/datascience/parton/conda/miniconda3/latest/setup.sh
-export LD_LIBRARY_PATH=/lus/theta-fs0/projects/datascience/parton/cuda/TensorRT-7.2.0.14/lib:/lus/theta-fs0/projects/datascience/parton/cuda/cudnn-8.0.4/lib64::/lus/theta-fs0/projects/datascience/parton/cuda/nccl_2.7.8-1+cuda11.1_x86_64/lib
+MCONDA=/lus/theta-fs0/software/thetagpu/conda/tf_master/2021-01-08/mconda3
+source $MCONDA/setup.sh
+
 # get current folder containing this script
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -LP)
 echo DIR=$DIR
@@ -21,8 +26,9 @@ echo PATH=$PATH
 
 NODES=`cat $COBALT_NODEFILE | wc -l`
 PPN=8
-PROCS=$((NODES * PPN))
-echo NODES=$NODES  PPN=$PPN  PROCS=$PROCS
+RANKS=$((NODES * PPN))
+echo NODES=$NODES  PPN=$PPN  RANKS=$RANKS
+export OMP_NUM_THREADS=8
 
 # MNIST expects the mnist.tgz file to be in $HOME/.keras/datasets
 if [ ! -d $HOME/.keras/datasets/mnist.tgz ]; then
@@ -30,7 +36,14 @@ if [ ! -d $HOME/.keras/datasets/mnist.tgz ]; then
    cp $DIR/../datasets/mnist.tgz $HOME/.keras/datasets/
 fi
 
-echo which mpirun = $(which mpirun)
-mpirun -hostfile $COBALT_NODEFILE -n $PROCS -npernode $PPN  \
-    python $DIR/../main.py -c $DIR/../configs/mnist.json \
-    --logdir $DIR/../logdir/$COBALT_JOBID --intraop 16 --interop 16 --horovod
+EXEC=$(which python)
+if [ $RANKS -gt 1 ]; then
+   echo [$SECONDS] adding horovod with $RANKS ranks
+   HOROVOD=--horovod
+   EXEC="mpirun -n $RANKS -npernode $PPN -hostfile $COBALT_NODEFILE $(which python)"
+fi
+
+export TF_XLA_FLAGS=--tf_xla_auto_jit=1
+
+$EXEC main.py -c $DIR/../configs/mnist.json \
+    --logdir logdir/$COBALT_JOBID --intraop $OMP_NUM_THREADS --interop $OMP_NUM_THREADS $HOROVOD
